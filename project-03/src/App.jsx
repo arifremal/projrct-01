9a2e681623ad7bef4b1ec2eaf91294f52d43a56d
Ariful Islam <remalarif0130@gmail.com>
2025-07-02 12:43:58 +0600

    added

---
+++ project-03/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Card from './Card.jsx'
+function App() {
+ 
+
+  return (
+  
+    <>
+    <Card username="ariff" btnT="visit"></Card>
+    <Card username="ariff remal" btnT="visit us"></Card>
+    <Card username="ariff remal" ></Card>
+ 
+
+    </>
+  )
+}
+
+export default App
